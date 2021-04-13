79a3009fad441636483fb3827ee9979c5f795de0
Developer <Developer@gmail.com>
2021-04-13 09:48:33 +0530

    changed Streams.txt

--- Streams/src/com/mangesh/sequentialvsparallel/ParallelStreams.java
+++ Streams/src/com/mangesh/sequentialvsparallel/ParallelStreams.java
@@ -4,18 +4,20 @@ import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
 import java.util.stream.IntStream;
 
-public class SequentialStreams{
+public class ParallelStreams {
 
         public static void main(String[] args) {
+            System.out.println("Thread NAme:"+ Thread.currentThread().getName());
         System.out.println(isPrime(6));
         LocalTime t1,t2;
         t1 = LocalTime.now();
-        IntStream.rangeClosed(1,500000).filter(SequentialStreams::isPrime).forEach(System.out :: println);
+        IntStream.rangeClosed(1,500000).parallel().filter(ParallelStreams::isPrime).forEach(System.out :: println);
         t2 = LocalTime.now();
-        System.out.println("Time taken for execution in seconds : "+t1.until(t2, ChronoUnit.SECONDS));//35 seconds
+        System.out.println("Time taken for execution in seconds : "+t1.until(t2, ChronoUnit.SECONDS));//18 seconds
 
     }
     public static boolean isPrime(int number){
+        System.out.println("Thread NAme:"+ Thread.currentThread().getName());
         if (number <=1) return false;
         return !IntStream.rangeClosed(2,number/2).filter(n -> number%n == 0).findAny().isPresent();
     }
--- Streams/src/com/mangesh/sequentialvsparallel/SequentialStreams.java
+++ Streams/src/com/mangesh/sequentialvsparallel/SequentialStreams.java
@@ -7,15 +7,17 @@ import java.util.stream.IntStream;
 public class SequentialStreams{
 
         public static void main(String[] args) {
+            System.out.println("Thread NAme:"+ Thread.currentThread().getName());
         System.out.println(isPrime(6));
         LocalTime t1,t2;
         t1 = LocalTime.now();
         IntStream.rangeClosed(1,500000).filter(SequentialStreams::isPrime).forEach(System.out :: println);
         t2 = LocalTime.now();
-        System.out.println("Time taken for execution in seconds : "+t1.until(t2, ChronoUnit.SECONDS));
+        System.out.println("Time taken for execution in seconds : "+t1.until(t2, ChronoUnit.SECONDS));//35 seconds
 
     }
     public static boolean isPrime(int number){
+        System.out.println("Thread NAme:"+ Thread.currentThread().getName());
         if (number <=1) return false;
         return !IntStream.rangeClosed(2,number/2).filter(n -> number%n == 0).findAny().isPresent();
     }
